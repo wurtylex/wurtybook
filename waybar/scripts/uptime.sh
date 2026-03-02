2f6842c8c290a0c8c91e3ddc236249e61be30098
wurtylex <anthony.c.chang10@gmail.com>
2026-03-02 16:59:31 -0500

    fix cpu spacing on waybar, removed icon and switched update timing for uptime on waybar

--- waybar/scripts/uptime.sh
+++ waybar/scripts/uptime.sh
@@ -3,4 +3,4 @@ UPTIME_PRETTY=$(uptime -p)
 
 UPTIME_FORMATTED=$(echo "$UPTIME_PRETTY"| sed 's/^up //;s/,*$//;s/minute/m/; s/hour/h/; s/day/d/; s/s//g')
 
-echo " $UPTIME_FORMATTED"
+echo "$UPTIME_FORMATTED"

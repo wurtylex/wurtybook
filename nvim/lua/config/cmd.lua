bed367c4a58988d7edccc7dbe8d49aeaf7e3afd4
wurtylex <anthony.c.chang10@gmail.com>
2026-07-01 20:30:42 -0700

    restore cursor to last position when reopening a tex file

--- nvim/lua/config/cmd.lua
+++ nvim/lua/config/cmd.lua
@@ -10,3 +10,16 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.hl.on_yank()
   end,
 })
+
+vim.api.nvim_create_autocmd('BufReadPost', {
+  desc = 'Restore cursor to last position when reopening a tex file',
+  group = vim.api.nvim_create_augroup('restore-cursor', { clear = true }),
+  pattern = { '*.tex', '*.latex' },
+  callback = function(args)
+    local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+    local line_count = vim.api.nvim_buf_line_count(args.buf)
+    if mark[1] > 0 and mark[1] <= line_count then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})

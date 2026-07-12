fe5f8f32007df03763b92ab0848a1851031ed45f
wurtylex <anthony.c.chang10@gmail.com>
2026-07-12 15:21:41 -0700

    made it so i only have to quit one lean file to quit the entire lean

--- nvim/lua/plugin/lean/init.lua
+++ nvim/lua/plugin/lean/init.lua
@@ -15,4 +15,18 @@ return {
   opts = { -- see the manual for full configuration options
     mappings = true,
   },
+
+  config = function(_, opts)
+    require('lean').setup(opts)
+
+    -- Closing a Lean buffer with :q / :wq leaves the infoview split open,
+    -- forcing a second :q. Close the infoview just before quitting so one
+    -- command tears down both windows.
+    vim.api.nvim_create_autocmd('QuitPre', {
+      pattern = '*.lean',
+      callback = function()
+        require('lean.infoview').close_all()
+      end,
+    })
+  end,
 }

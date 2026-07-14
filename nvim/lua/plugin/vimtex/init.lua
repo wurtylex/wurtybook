c7eb20e5f0eb1dc59e74c3c233a960d5cea17644
wurtylex <anthony.c.chang10@gmail.com>
2026-07-13 23:33:18 -0700

    keep build files for latex hidden

--- nvim/lua/plugin/vimtex/init.lua
+++ nvim/lua/plugin/vimtex/init.lua
@@ -4,6 +4,9 @@ return {
   init = function()
     vim.g.vimtex_view_method = 'zathura'
     vim.g.vimtex_compiler_method = 'latexmk'
+    vim.g.vimtex_compiler_latexmk = {
+      aux_dir = '.build',
+    }
     vim.g.vimtex_quickfix_ignore_filters = {
       '^Underfull',
       '^Overfull',

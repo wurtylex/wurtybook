d21359721e1690bbf567251a76a99d42b88a3896
anthony.c.chang10@gmail.com <anthony.c.chang10@gmail.com>
2026-08-14 22:39:10 +0800

    pushed changed to nvim to use skim on macbook rather than zathura

--- nvim/lua/plugin/vimtex/init.lua
+++ nvim/lua/plugin/vimtex/init.lua
@@ -2,7 +2,14 @@ return {
   'lervag/vimtex',
   lazy = false,
   init = function()
-    vim.g.vimtex_view_method = 'zathura'
+    -- PDF viewer: Skim on macOS (SyncTeX forward/backward search), zathura on Linux.
+    if vim.fn.has 'mac' == 1 then
+      vim.g.vimtex_view_method = 'skim'
+      vim.g.vimtex_view_skim_sync = 1 -- forward search after compile
+      vim.g.vimtex_view_skim_activate = 1 -- focus Skim on forward search
+    else
+      vim.g.vimtex_view_method = 'zathura'
+    end
     vim.g.vimtex_compiler_method = 'latexmk'
     vim.g.vimtex_compiler_latexmk = {
       aux_dir = '.build',
